c0df3c49123be4a404d7ee548d6e2e0e53c8bad5
barcelona2004 <barcelona200418@yandex.ru>
2023-03-30 21:39:53 +0700

    first step

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import argparse
+
+parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+parser.add_argument("first_file")
+parser.add_argument("second_file")
+parser.parse_args()

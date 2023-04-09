23d4d29e9d6405c9bae5f5e4903b6f382959bb7c
Maksim Gordt <barcelona200418@yandex.ru>
2023-04-09 18:17:46 +0700

    try to connect test coverage

--- Makefile
+++ Makefile
@@ -10,3 +10,5 @@ redo:
 	pip install --user --force-reinstall dist/*.whl
 lint:
 	poetry run flake8 gendiff
+test-coverage:
+	poetry run pytest --cov=hexlet_python_package --cov-report xml
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,4 +1,4 @@
-from gendiff.generate_diff import generate_diff
+from gendiff import generate_diff
 
 def test_generate_diff():
     assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == '{\n-host: 1\n+host: 2\n}'

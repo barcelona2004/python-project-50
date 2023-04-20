103ee1d9b0e1d411537b42993a5cf5ab3151794c
Maksim Gordt <barcelona200418@yandex.ru>
2023-04-20 11:56:38 +0700

    fixing mistakes with files paths

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -3,12 +3,12 @@ import yaml
 import os
 
 
-def get_data(path: str) -> dict:
-    f = open(path)
-    _, extension = os.path.splitext(path)
+def get_data(file_path: str) -> dict:
+    f = open(file_path)
+    _, extension = os.path.splitext(file_path)
     if extension == '.json':
         return json.load(f)
     elif extension == '.yaml' or extension == '.yml':
         return yaml.safe_load(f)[0]
     else:
-        raise ValueError(f"Wrong extenssion {extension}")
+        raise ValueError(f"Wrong extension {extension}")

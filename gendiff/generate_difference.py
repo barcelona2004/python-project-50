19b26042fbc25b171745cbd461b7438c97185b34
Maksim Gordt <barcelona200418@yandex.ru>
2023-04-20 12:21:42 +0700

    fixing mistakes with files paths 2.0

--- gendiff/generate_difference.py
+++ gendiff/generate_difference.py
@@ -1,10 +1,28 @@
 from gendiff.tree import get_diff
 from gendiff.parser import get_data
 from gendiff.formatters.formatt import formatt
+import os
+
+
+def kost(path: str) -> str:
+    new_path = path.split('/')
+    new_string = ''
+    count = 0
+    for i in new_path:
+        if i != "scripts":
+            if i == "gendiff":
+                if count < 1:
+                    new_string += f"{i}/"
+                    count += 1
+            else:
+                new_string += f"{i}/"
+    return new_string[:-1]
 
 
 def generate_diff(file_path1: str, file_path2: str, format_name="stylish"):
-    file1 = get_data(file_path1)
-    file2 = get_data(file_path2)
+    file_path1_abs = kost(os.path.abspath(file_path1))
+    file_path2_abs = kost(os.path.abspath(file_path2))
+    file1 = get_data(file_path1_abs)
+    file2 = get_data(file_path2_abs)
     result = get_diff(file1, file2)
     return formatt(result, format_name)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 from gendiff.cli import parse_cli_args
 from gendiff import generate_diff
-import os
 
 
 def main():
     path1, path2, format_name = parse_cli_args()
-    path11 = os.path.abspath(path1)
-    path22 = os.path.abspath(path2)
-    print(generate_diff(path11, path22, format_name))
+    print(generate_diff(path1, path2, format_name))
 
 
 if __name__ == '__main__':

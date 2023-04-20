8c421d4484100ed0a33290a8d565c0177f12e8fb
Maksim Gordt <barcelona200418@yandex.ru>
2023-04-20 09:17:21 +0700

    fixing parser

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,13 +1,18 @@
 import json
 import yaml
 import os
+from yaml.loader import SafeLoader
 
 
 def get_data(path: str) -> dict:
     _, extension = os.path.splitext(path)
     if extension == '.json':
-        return json.load(open(f'{path}'))
+        with open(path) as f:
+            data = json.load(f)
+            return data
     elif extension == '.yaml' or extension == '.yml':
-        return yaml.safe_load(open(f'{path}'))[0]
+        with open(path) as f:
+            data = yaml.load(f, Loader=SafeLoader)
+            return data[0]
     else:
         raise ValueError(f"Wrong extension {extension}")
